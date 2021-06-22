89e0019d3196f889e7da28968e8ff4b0a365827a
Gene Romuga <generomuga12693@gmail.com>
2021-06-22 21:43:36 +0800

    Add signup form

---
+++ src/Signup.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default class Signup extends Component {
+
+    constructor(props){
+        super(props)
+    }
+    
+    render(){
+        return (
+            <View >
+                <Text>Signup</Text>
+            </View>            
+        )
+    }
+
+}
