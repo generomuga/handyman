d2cbd79ec11d910e03ff598da6222acbffe92d33
Gene Romuga <generomuga12693@gmail.com>
2021-06-22 21:08:52 +0800

    Reformat code and do some refactorings

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import Login from './src/Login';
 import Home from './src/Home';
+
 import { AntDesign } from '@expo/vector-icons';
 
 import * as firebase from 'firebase';
@@ -13,40 +15,38 @@ export default class App extends Component {
   render() {
 
     return (
+
         <NavigationContainer>
-          
             <Stack.Navigator initialRouteName='Login'>
-                <Stack.Screen name='Login' component={Login} options= {{headerShown: false}} />
                 <Stack.Screen 
-                  name='Home' 
-                  component={Home} 
-                  options= {
-                    {
-                      headerShown: true, 
-                      title: 'Hugefort Handyman +', 
-                      headerLeft: ()=> null,
-                      headerRight: ()=> (
-                          <View>
-                              {/* <Text onPress={()=>firebase.auth().signOut().then(()=>{}).catch((error) =>{})}>Sign out</Text> */}
-                              <AntDesign 
-                                name="logout" 
-                                size={24} color="#FAFAFA" 
-                                style={style.iconStyle} 
-                                onPress={()=>firebase.auth().signOut().then(()=>{}).catch((error) =>{})}
-                                />
-                          </View>
-                      ),
-                      headerStyle: {
-                        backgroundColor: '#039BE5'
-                      },
-                      headerTintColor: '#FAFAFA'
-                    }
-                  } 
-            />
-
+                    name='Login' 
+                    component={Login} 
+                    options= {{headerShown: false}} />
+                <Stack.Screen 
+                    name='Home' 
+                    component={Home} 
+                    options= {{
+                          headerShown: true, 
+                          title: 'Handyman Plus', 
+                          headerLeft: ()=> null,
+                          headerRight: ()=> (
+                              <View>
+                                  <AntDesign 
+                                    name="logout" 
+                                    size={24}
+                                    color="#FAFAFA" 
+                                    style={{margin:15}} 
+                                    onPress={()=>firebase.auth().signOut().then(()=>{}).catch((error) =>{})} />
+                              </View>
+                          ),
+                          headerStyle: {
+                            backgroundColor: '#039BE5'
+                          },
+                          headerTintColor: '#FAFAFA'
+                    }} />
             </Stack.Navigator>
-
         </NavigationContainer>
+
     );
 
   }
@@ -54,11 +54,3 @@ export default class App extends Component {
 }
 
 const Stack = createStackNavigator();
-
-const style = StyleSheet.create({
-
-  iconStyle: {
-    marginRight: 15
-  }
-
-})
