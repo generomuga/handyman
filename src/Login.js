2a6a5e4c212f4ec753d74b2140a0d27a236b2828
Gene Romuga <generomuga12693@gmail.com>
2021-06-22 21:18:26 +0800

    Reformat code and do some refactorings

--- src/Login.js
+++ src/Login.js
@@ -20,167 +20,160 @@ export default class Login extends Component {
     }
   
     fbLogIn = async() => {
-      try {
-        await Facebook.initializeAsync({
-          appId: '489615325448707',
-        });
-        const {
-          type,
-          token
-          // expirationDate,
-          // permissions,
-          // declinedPermissions,
-        } = await Facebook.logInWithReadPermissionsAsync({
-          permissions: ['public_profile'],
-        });
-        if (type === 'success') {
-          // Get the user's name using Facebook's Graph API
-          const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-          // #console.log((await response.json()).appId)
-          Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
-        
-          const credential = firebase.auth.FacebookAuthProvider.credential(token)
-          
-          // firebase.auth().signInWithCredential(credential).catch((error) => {
-          //   console.log(error)
-          // })
-
-        } else {
-          // type === 'cancel'
+        try {
+          await Facebook.initializeAsync({
+            appId: '489615325448707',
+          });
+          const {
+            type,
+            token
+            // expirationDate,
+            // permissions,
+            // declinedPermissions,
+          } = await Facebook.logInWithReadPermissionsAsync({
+            permissions: ['public_profile'],
+          });
+          if (type === 'success') {
+            // Get the user's name using Facebook's Graph API
+            const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+            // #console.log((await response.json()).appId)
+            Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+            const credential = firebase.auth.FacebookAuthProvider.credential(token)
+            // firebase.auth().signInWithCredential(credential).catch((error) => {
+            //   console.log(error)
+            // })
+          } else {
+            // type === 'cancel'
+          }
+        } catch ({ message }) {
+          alert(`Facebook Login Error: ${message}`);
         }
-      } catch ({ message }) {
-        alert(`Facebook Login Error: ${message}`);
-      }
     }
 
     checkLoginState = (response) => {
-      
-      if (response.authResponse) {
-        // User is signed-in Facebook.
-        var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
-          unsubscribe();
-          
-          // Check if we are already signed-in Firebase with the correct user.
-          if (!this.isUserEqualFacebook(response.authResponse, firebaseUser)) {
-            // Build Firebase credential with the Facebook auth token.
-            var credential = firebase.auth.FacebookAuthProvider.credential(
-                response.authResponse.accessToken);
-    
-            // Sign in with the credential from the Facebook user.
-            firebase.auth().signInWithCredential(credential)
-              .catch((error) => {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                // ...
-              });
-          } else {
-            // User is already signed-in Firebase with the correct user.
-          }
-        });
-      } else {
-        // User is signed-out of Facebook.
-        // firebase.auth().signOut();
-        // console.log('Eyys')
-      }
+        if (response.authResponse) {
+          // User is signed-in Facebook.
+          var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+            unsubscribe(); 
+            // Check if we are already signed-in Firebase with the correct user.
+            if (!this.isUserEqualFacebook(response.authResponse, firebaseUser)) {
+                // Build Firebase credential with the Facebook auth token.
+                var credential = firebase.auth.FacebookAuthProvider.credential(response.authResponse.accessToken);
+                // Sign in with the credential from the Facebook user.
+                firebase.auth().signInWithCredential(credential)
+                  .catch((error) => {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
+                    // ...
+                  });
+            } else {
+              // User is already signed-in Firebase with the correct user.
+            }
+          });
+        } 
+        else {
+          // User is signed-out of Facebook.
+          // firebase.auth().signOut();
+          // console.log('Eyys')
+        }
     }
 
     isUserEqualGoogle = (googleUser, firebaseUser) => {
-      if (firebaseUser) {
-        var providerData = firebaseUser.providerData;
-        for (var i = 0; i < providerData.length; i++) {
-          if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-              providerData[i].uid === googleUser.getBasicProfile().getId()) {
-            return true;
-          }
+        if (firebaseUser) {
+            var providerData = firebaseUser.providerData;
+            for (var i = 0; i < providerData.length; i++) {
+                if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                    providerData[i].uid === googleUser.getBasicProfile().getId()) {
+                    return true;
+                }
+            }
         }
-      }
-      return false;
+        return false;
     }
 
     isUserEqualFacebook = (facebookAuthResponse, firebaseUser) => {
-      if (firebaseUser) {
-        var providerData = firebaseUser.providerData;
-        for (var i = 0; i < providerData.length; i++) {
-          if (providerData[i].providerId === firebase.auth.FacebookAuthProvider.PROVIDER_ID &&
-              providerData[i].uid === facebookAuthResponse.userID) {
-            // We don't need to re-auth the Firebase connection.
-            return true;
-          }
+        if (firebaseUser) {
+            var providerData = firebaseUser.providerData;
+            for (var i = 0; i < providerData.length; i++) {
+                if (providerData[i].providerId === firebase.auth.FacebookAuthProvider.PROVIDER_ID &&
+                    providerData[i].uid === facebookAuthResponse.userID) {
+                    return true;
+                }
+            }
         }
-      }
-      return false;
+        return false;
     }
 
     onSignIn = googleUser => {
-      console.log('Google Auth Response', googleUser);
-      // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-      var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
-        unsubscribe();
-        // Check if we are already signed-in Firebase with the correct user.
-        if (!this.isUserEqualGoogle(googleUser, firebaseUser)) {
-          // Build Firebase credential with the Google ID token.
-          var credential = firebase.auth.GoogleAuthProvider.credential(
-              // googleUser.getAuthResponse().id_token);
-              googleUser.idToken,
-              googleUser.accessToken
-          );
-          
-          // Sign in with credential from the Google user.
-          firebase.auth().signInWithCredential(credential).catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-          });
-        } else {
-          console.log('User already signed-in Firebase.');
-        }
-      });
+        console.log('Google Auth Response', googleUser);
+        // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+        var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+            unsubscribe();
+            // Check if we are already signed-in Firebase with the correct user.
+            if (!this.isUserEqualGoogle(googleUser, firebaseUser)) {
+                // Build Firebase credential with the Google ID token.
+                var credential = firebase.auth.GoogleAuthProvider.credential(
+                    // googleUser.getAuthResponse().id_token);
+                    googleUser.idToken,
+                    googleUser.accessToken
+                );
+                // Sign in with credential from the Google user.
+                firebase.auth().signInWithCredential(credential).catch((error) => {
+                  // Handle Errors here.
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  // The email of the user's account used.
+                  var email = error.email;
+                  // The firebase.auth.AuthCredential type that was used.
+                  var credential = error.credential;
+                  // ...
+                });
+            } 
+            else {
+                console.log('User already signed-in Firebase.');
+            }
+        });
     }
 
     signInWithGoogleAsync = async() => {
-      try {
-          const result = await Google.logInAsync({
-            // androidClientId: YOUR_CLIENT_ID_HERE,
-            iosClientId: '199145126003-0nnmv1svb19unku0arss5vuq6q0r2kqo.apps.googleusercontent.com',
-            scopes: ['profile', 'email']}
-          );
-        
-          if (result.type === 'success') {
-            this.onSignIn(result);
-            return result.accessToken;
-          } else {
-            return { cancelled: true };
-          }
-      } 
-      catch (e) {
-        return { error: true };
-      }
+        try {
+            const result = await Google.logInAsync({
+              // androidClientId: YOUR_CLIENT_ID_HERE,
+              iosClientId: '199145126003-0nnmv1svb19unku0arss5vuq6q0r2kqo.apps.googleusercontent.com',
+              scopes: ['profile', 'email']}
+            );
+            if (result.type === 'success') {
+                this.onSignIn(result);
+                return result.accessToken;
+            } 
+            else {
+                return { cancelled: true };
+            }
+        } 
+        catch (e) {
+          return { error: true };
+        }
         
     }
 
     checkIfLoggedIn = () => {
-      firebase.auth().onAuthStateChanged(user => {
-          if (user) {
-              this.props.navigation.navigate('Home')
-          }
-          else {
-              this.props.navigation.navigate('Login')
-          }
-      }
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.props.navigation.navigate('Home')
+            }
+            else {
+                this.props.navigation.navigate('Login')
+            }
+        }
     )}
 
     constructor(props){
-      super(props)
+        super(props)
     }
 
     render(){
