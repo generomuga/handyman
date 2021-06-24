5fbe80ed3a2b2cc1172c8a05161128e0936fadd9
Gene Romuga <generomuga12693@gmail.com>
2021-06-24 17:19:53 +0800

    Add textinput style component

---
+++ src/styles/inputtext.js
@@ -0,0 +1,13 @@
+export const border = {
+    borderRadius: 20,
+    borderWidth: 3,
+    borderColor: '#FAFAFA'
+}
+
+export const padding = {
+    padding:10
+}
+
+export const color = {
+    backgroundColor: 'white'
+}
