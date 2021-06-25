2fd1953e2610e8aa7ad1e31ec0cffa687573b099
Gene Romuga <generomuga12693@gmail.com>
2021-06-25 21:41:40 +0800

    Activate headerleft button

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default class App extends Component {
                     options= {{
                           headerShown: true, 
                           title: 'Handyman Plus', 
-                          headerLeft: ()=> null,
+                          // headerLeft: ()=> null,
                           headerRight: ()=> (
                               <View>
                                   <AntDesign 
