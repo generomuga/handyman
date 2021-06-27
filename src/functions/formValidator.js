da6945c8c6852cc8050da729d0a1a122a20679e8
Gene Romuga <generomuga12693@gmail.com>
2021-06-27 09:24:05 +0800

    Add class for form validator

---
+++ src/functions/formValidator.js
@@ -0,0 +1,58 @@
+class FormValidator {
+
+    isEmailEmpty(text) {
+        if (text.trim() === '') {
+            // console.log('Empty email new')
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    isNotValidEmail(text) {
+        let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (pattern.test(text) === false) {
+            // console.log('Invalid email new')
+            return true;
+        }
+        else {
+            return false
+        }
+    }
+
+    isPasswordEmpty(text) {
+        if (text.trim() === '') {
+            // console.log('Empty password new')
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    isNotValidPassword(text) {
+        if (text.length < 8 || text.trim() === '') {
+            // console.log('Invalid password new')
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    isNotSamePassword(password,rePassword) {
+        if (password != rePassword) {
+            // console.log('Password didnt match new')
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+}
+
+const formValidator = new FormValidator();
+
+export default formValidator;
