4d1dc17f5ab1c791f9424e53ffa46fe3ae3501e1
Gene Romuga <generomuga12693@gmail.com>
2021-06-27 21:28:30 +0800

    Add Me Tab

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-
 function HomeTab() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -28,6 +27,14 @@ function NotificationTab() {
     );
 }
 
+function MeTab() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Me!</Text>
+        </View>
+    );
+}
+
 const Tab = createBottomTabNavigator();
 
 export default class Home extends Component {
@@ -38,6 +45,7 @@ export default class Home extends Component {
                 <Tab.Screen name="Home" component={HomeTab} />
                 <Tab.Screen name="Book" component={BookTab} />
                 <Tab.Screen name="Notication" component={NotificationTab} />
+                <Tab.Screen name="Me" component={MeTab} />
             </Tab.Navigator>
         )
     }
