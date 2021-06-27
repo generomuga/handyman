8ee6673ef00c82f5a6d6d6cacc9156842fb27eb2
Gene Romuga <generomuga12693@gmail.com>
2021-06-27 21:07:07 +0800

    Add tab navigator

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+function HomeTab() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+  
+function BookTab() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Book!</Text>
+        </View>
+    );
+}
+
+function NotificationTab() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Notification!</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default class Home extends Component {
         
     render(){
         return (
-            <View >
-                <Text>Hello</Text>
-            </View>            
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeTab} />
+                <Tab.Screen name="Book" component={BookTab} />
+                <Tab.Screen name="Notication" component={NotificationTab} />
+            </Tab.Navigator>
         )
     }
 
