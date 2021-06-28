1c571e0f17b4664a48e5355809cbb574bbd4e428
Gene Romuga <generomuga12693@gmail.com>
2021-06-28 21:03:12 +0800

    Add sample components

--- src/BookTab.js
+++ src/BookTab.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Picker } from 'react-native';
 
 export default class BookTab extends Component {
         
     render(){
         return (
             <View>
-                <Text>Book</Text>
+                <Text>Let's do it for you</Text>
             </View>
         )
     }
--- src/HomeTab.js
+++ src/HomeTab.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList } from 'react-native';
+
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'First Item',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Second Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+  ];
+
+const Item = ({ title }) => (
+<View>
+    <Text>{title}</Text>
+</View>
+);
+
+const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
 
 export default class HomeTab extends Component {
-        
+   
+
     render(){
         return (
             <View>
-                <Text>Home</Text>
+                <FlatList
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                />
             </View>
         )
     }
