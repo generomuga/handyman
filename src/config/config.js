e4daa0223f81b798d34e5947fc2b224e5e7a9b1a
Gene Romuga <generomuga12693@gmail.com>
2021-06-28 18:05:10 +0800

    Add database url

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,7 @@ export const firebaseConfig = {
     projectId: "handyman-plus",
     storageBucket: "handyman-plus.appspot.com",
     messagingSenderId: "876177652588",
+    databaseURL: 'https://handyman-plus-default-rtdb.asia-southeast1.firebasedatabase.app/',
     appId: "1:876177652588:web:97033d6b546bef8d6c5149",
     measurementId: "G-8FKV7YLBCQ"
 };
