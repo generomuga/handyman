248a82f6499ca9e8b297defa928bcfcaf47e8910
Gene Romuga <generomuga12693@gmail.com>
2021-06-28 20:40:10 +0800

    Add tab navigation icons

--- src/Home.js
+++ src/Home.js
@@ -8,19 +8,83 @@ import BookTab from './BookTab';
 import NotificationTab from './NotifcationTab';
 import MeTab from './MeTab';
 
+import { Entypo } from '@expo/vector-icons'; 
+
 const Tab = createBottomTabNavigator();
 
 export default class Home extends Component {
         
     render(){
         return (
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeTab} />
-                <Tab.Screen name="Book" component={BookTab} />
-                <Tab.Screen name="Notication" component={NotificationTab} />
-                <Tab.Screen name="Me" component={MeTab} />
+            <Tab.Navigator
+                tabBarOptions={{
+                    showLabel:false,
+                    inactiveTintColor: '#E1F5FE',
+                    activeTintColor: 'white',
+                    
+                    style: {
+                        backgroundColor:'#039BE5',
+                        // position: 'absolute',
+                        // bottom: 20,
+                        // marginHorizontal: 5,
+                        // borderRadius: 25
+                    },
+
+                }}
+                >
+
+                <Tab.Screen 
+                    name="Home" 
+                    component={HomeTab} 
+                    options={{
+                        tabBarIcon: ({focused}) => (
+                            <View style={{justifyContent:'center', alignContent:'center', top:10}}>
+                                <Entypo name="home" size={24} style={{color:focused?'white':'#B3E5FC',textAlign:'center'}}  />
+                                <Text style={{color:focused?'white':'#B3E5FC', fontSize:12, textAlign:'center'}}>Home</Text>
+                            </View>
+                        )
+                    }}
+                    />
+
+                <Tab.Screen 
+                    name="Book" 
+                    component={BookTab} 
+                    options={{
+                        tabBarIcon: ({focused}) => (
+                            <View style={{justifyContent:'center', alignContent:'center', top:10}}>
+                                <Entypo name="book" size={24} style={{color:focused?'white':'#B3E5FC', textAlign:'center'}}  />
+                                <Text style={{color:focused?'white':'#B3E5FC', fontSize:12, textAlign:'center'}}>Book</Text>
+                            </View>
+                        )
+                    }}
+                    />
+
+                <Tab.Screen 
+                    name="Notication" 
+                    component={NotificationTab} 
+                    options={{
+                        tabBarIcon: ({focused}) => (
+                            <View style={{justifyContent:'center', alignContent:'center', top:10}}>
+                                <Entypo name="notification" size={24} style={{color:focused?'white':'#B3E5FC', textAlign:'center'}}  />
+                                <Text style={{color:focused?'white':'#B3E5FC', fontSize:12, textAlign:'center'}}>Notications</Text>
+                            </View>
+                        )
+                    }}
+                    />
+
+                <Tab.Screen 
+                    name="Me" 
+                    component={MeTab} 
+                    options={{
+                        tabBarIcon: ({focused}) => (
+                            <View style={{justifyContent:'center', alignContent:'center', top:10}}>
+                                <Entypo name="user" size={24} style={{color:focused?'white':'#B3E5FC', textAlign:'center'}}  />
+                                <Text style={{color:focused?'white':'#B3E5FC', fontSize:12, textAlign:'center'}}>Me</Text>
+                            </View>
+                        )
+                    }}/>
             </Tab.Navigator>
         )
     }
 
-}
+} 
