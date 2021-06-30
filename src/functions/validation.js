a178bbd9aa50c4355d6b50124a9fdea07fb5c4da
Gene Romuga <generomuga12693@gmail.com>
2021-06-30 19:11:57 +0800

    Add validation class

---
+++ src/functions/validation.js
@@ -0,0 +1,58 @@
+class Validation {
+
+    isEmailEmpty(text) {
+        if (text.trim() === '') {
+            // console.log('Empty email new')
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    isNotValidEmail(text) {
+        let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (pattern.test(text) === false) {
+            // console.log('Invalid email new')
+            return true;
+        }
+        else {
+            return false
+        }
+    }
+
+    isPasswordEmpty(text) {
+        if (text.trim() === '') {
+            // console.log('Empty password new')
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    isNotValidPassword(text) {
+        if (text.length < 8 || text.trim() === '') {
+            // console.log('Invalid password new')
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    isNotSameText(text1,text2) {
+        if (text1 != text2) {
+            // console.log('Password didnt match new')
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+}
+
+const validation = new Validation();
+
+export default validation;
