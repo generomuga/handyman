8d17a319d6d45e4c3b8d388da913b4f4d6a7115a
Gene Romuga <generomuga12693@gmail.com>
2021-07-01 21:48:07 +0800

    Add other profile info

--- src/MeTab.js
+++ src/MeTab.js
@@ -28,6 +28,7 @@ export default class MeTab extends Component {
             if (snapshot.exists()) {
               const data = snapshot.val();
               this.setState({displayName:data['displayName']});
+              this.setState({email:data['email']});
             } else {
               console.log("No data available");
             }
@@ -43,6 +44,8 @@ export default class MeTab extends Component {
 
         var updates = {};
         updates['displayName'] = this.state.displayName;
+        updates['contactNo'] = this.state.contactNo;
+        updates['address'] = this.state.address;
 
         dbRef.child("users").child(userId).update(updates);
 
@@ -52,14 +55,21 @@ export default class MeTab extends Component {
      
         const {isDisplayNameEditable, buttonLabel } = this.state;
 
-        if (isDisplayNameEditable === 'true') {
-            this.setState({isDisplayNameEditable:'false'});
+        if (isDisplayNameEditable === true) {
+            this.setState({isDisplayNameEditable:false});
+            this.setState({isContactNoEditable:false});
+            this.setState({isAddressEditable:false});
+
             this.setState({buttonLabel:'Login'});
+
             this.updateUserDetails();
             
         }
         else {
-            this.setState({isDisplayNameEditable:'true'});
+            this.setState({isDisplayNameEditable:true});
+            this.setState({isContactNoEditable:true});
+            this.setState({isAddressEditable:true});
+
             this.setState({buttonLabel:'Save'})
             
         }
@@ -75,7 +85,13 @@ export default class MeTab extends Component {
 
         this.state = {
             displayName: '',
-            isDisplayNameEditable: 'false',
+            email: '',
+            contactNo: '',
+            address: '',
+            isDisplayNameEditable: false,
+            isEmailEditable: false,
+            isContactNoEditable: false,
+            isAddressEditable: false,
             buttonLabel: 'Login'
         }
 
@@ -87,11 +103,38 @@ export default class MeTab extends Component {
 
                 <TextInput 
                     // style={style.textInput} 
-                    placeholder='email' 
+                    placeholder='full name' 
                     autoCapitalize='none' 
                     value={this.state.displayName}
                     editable={this.state.isDisplayNameEditable}
                     onChangeText={displayName => this.setState({displayName})}
+                    />
+
+                <TextInput 
+                    // style={style.textInput} 
+                    placeholder='email' 
+                    autoCapitalize='none' 
+                    value={this.state.email}
+                    editable={this.state.isEmailEditable}
+                    onChangeText={email => this.setState({email})}
+                    />
+
+                <TextInput 
+                    // style={style.textInput} 
+                    placeholder='contact number' 
+                    autoCapitalize='none' 
+                    value={this.state.contactNo}
+                    editable={this.state.isContactNoEditable}
+                    onChangeText={contactNo => this.setState({contactNo})}
+                    />
+
+                <TextInput 
+                    // style={style.textInput} 
+                    placeholder='address' 
+                    autoCapitalize='none' 
+                    value={this.state.address}
+                    editable={this.state.isAddressEditable}
+                    onChangeText={address => this.setState({address})}
                     />
 
                 <TouchableOpacity 
