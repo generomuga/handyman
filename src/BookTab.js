862ba83cefe46358e0f6053823ce38181bfc6f5d
Gene Romuga <generomuga12693@gmail.com>
2021-07-03 17:36:21 +0800

    Add picker

--- src/BookTab.js
+++ src/BookTab.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text, Button, Picker } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default class BookTab extends Component {
         
     render(){
         return (
             <View>
-                <Text>Let's do it for you</Text>
+                <ModalDropdown 
+                    defaultValue="Select category..."
+                    options={['1','2']} >
+                </ModalDropdown>
+
+                <ModalDropdown 
+                    defaultValue="Select service..."
+                    options={['Service']} >
+                </ModalDropdown>
+
+                <ModalDropdown 
+                    defaultValue="Payment method..."
+                    options={['Service']} >
+                </ModalDropdown>
             </View>
         )
     }
