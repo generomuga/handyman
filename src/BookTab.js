0fc29a523562eaefde70bf8a51c2ac2be0f00e40
Gene Romuga <generomuga12693@gmail.com>
2021-07-03 22:09:09 +0800

    Add function to load data to dropdown

--- src/BookTab.js
+++ src/BookTab.js
@@ -3,14 +3,58 @@ import { View, Text, Button } from 'react-native';
 import ModalDropdown from 'react-native-modal-dropdown';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
+import * as firebase from 'firebase';
+
 export default class BookTab extends Component {
         
+    componentDidMount(){
+        const items = this.getList();
+        this.setState({category:items});
+    }
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            category: [],
+        }
+
+    }
+
+    getList() {
+
+        const dbRef = firebase.database().ref();
+
+        var items = []
+        dbRef.child('tenant/categories').get()                        
+            .then(snapshot => {
+                if (snapshot.exists()) {
+
+                    snapshot.forEach(function(childsnap){
+                        var key = childsnap.key;
+                        var data = childsnap.val();
+                        // var ref = db.child(key);
+                        items.push(data);
+                        // console.log(data);
+                        console.log(items);
+                    }
+                    )}
+                else {
+                    console.log('user not found');
+                }
+            });
+
+        console.log(items);
+        return items;
+
+    }
+
     render(){
         return (
             <View>
                 <ModalDropdown 
                     defaultValue="Select category..."
-                    options={['1','2']} >
+                    options={this.state.category} >
                 </ModalDropdown>
 
                 <ModalDropdown 
