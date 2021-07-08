6be9b7ebda6e7cac1fb7fe8263d99c696e800823
Gene Romuga <generomuga12693@gmail.com>
2021-07-08 11:10:26 +0800

    Add stylings on booking

--- src/BookTab.js
+++ src/BookTab.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, Button, SafeAreaView, TouchableOpacity, FlatList, Alert} from 'react-native';
+import { View, Text, Button, SafeAreaView, ScrollView, TouchableOpacity, FlatList, Alert} from 'react-native';
 // import DateTimePicker from '@react-native-community/datetimepicker';
 import RNPickerSelect from 'react-native-picker-select';
 import DateTimePicker from "react-native-modal-datetime-picker";
 
-import { LogBox } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
 
+import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['Setting a timer']);
 
 import * as firebase from 'firebase';
@@ -161,14 +162,51 @@ export default class BookTab extends Component {
     };
     
     renderItemComponent = (data) =>
-        <View style={{backgroundColor:''}}>
+        <View style={{
+                backgroundColor:'#FAFAFA', 
+                borderRadius:10, 
+                borderWidth:2, 
+                borderColor:'#F44336',
+                marginLeft: 20, 
+                marginRight: 20,
+                marginBottom:10,
+                flex:1,
+                justifyContent:'center'
+            }}>
+            <AntDesign 
+                style={{textAlign:'right', position:'relative', marginTop:5, marginRight:5}}
+                name="closecircle" 
+                size={24} 
+                color="#F44336" 
+                onPress={()=>{
+                    const dbRef = firebase.database().ref();
+                    const user = firebase.auth().currentUser;
+    
+                    var items = []
+                    dbRef.child('bookings/'+user['uid']+'/'+data.item.id).remove()                     
+                        .then(()=>{
+                            console.log("DELETED");
+                            
+                            const filteredData = this.state.tempBookValue.filter(item => item.id !== id);
+                            this.setState({ tempBookValue: filteredData });
+                            
+                        })
+                    this.getTempBooking();
+                }}
+                />
+            
             <Text>{data.item.id}</Text>
+            
             <Text>Category: {data.item.category}</Text>
+            
             <Text>Service: {data.item.service}</Text>
+            
             <Text>Date of service: {data.item.service_date}</Text>
+            
             <Text>Price: {String(data.item.service_currency).toUpperCase()} {data.item.service_price}</Text>
             {/* <Text>{data.item.id}</Text> */}
-            <Button title="Delete" onPress={()=>{
+            
+            {/* <Button title="Delete" onPress={()=>{
                 const dbRef = firebase.database().ref();
                 const user = firebase.auth().currentUser;
 
@@ -182,146 +220,155 @@ export default class BookTab extends Component {
                         
                     })
                 this.getTempBooking();
-            }}/>
+            }}/> */}
         </View>
 
     render(){
         return (
             <SafeAreaView>
 
-                <Text>Category</Text>
-                <RNPickerSelect
-                    onValueChange={(value) => {
-                        this.setState({categoryValue:value})
-                        // this.setState({serviceValue:''});
-                        this.getServiceList(value);
-                        // const listService = this.getServiceList(value);
-                        // this.setState({services:listService});
-                    }}
-                    items={this.state.categories}
-                >
-                    <Text>{this.state.categoryValue?this.state.categoryValue:'Select an item...'}</Text>
-                </RNPickerSelect>
+                <ScrollView>
 
-                <Text>Service</Text>
-                <RNPickerSelect
-                    onValueChange={(value) => {
-                        console.log(value);
-                        this.setState({serviceValue:value});
-                    }}
-                    items={this.state.services}
-                >
-                    <Text>{this.state.serviceValue?this.state.serviceValue:'Select an item...'}</Text>
-                </RNPickerSelect>
-
-                {/* <DateTimePicker
-                    style={{width:'100%', backgroundColor: 'white', position: 'absolute', bottom: 0, zIndex: 10}}
-                    testID="dateTimePicker"
-                    value={this.state.date}
-                    mode={this.state.mode}
-                    is24Hour={true}
-                    display="spinner"
-                    onChange={this.onChange}
-                    themeVariant="light"
-                    neutralButtonLabel="clear"
-                    /> */}
-
-                <Text>Date of service</Text>
-                <Text>{this.state.serviceDate?this.state.serviceDate:null}</Text>
-                <Button title="Set date of service" onPress={this.showDateTimePicker} />
-
-                <DateTimePicker
-                    isVisible={this.state.isDateTimePickerVisible}
-                    onConfirm={this.handleDatePicked}
-                    onCancel={this.hideDateTimePicker}
-                    display="default"
-                    />
-
-                <TouchableOpacity 
-                        // style={{}}
-                        onPress={()=>{
-
-                            this.setState({errorMsg:''})
-
-                            // console.log("date",this.state.serviceDate);  
-                            if (this.state.categoryValue===null){
-                                console.log("Di pde");
-                                this.setState({errorMsg:"* Please select category"})
-                                return
-                            }
-                            else if (this.state.serviceValue===null){
-                                console.log("Di pde");
-                                this.setState({errorMsg:"* Please select service"})
-                                return
-                            }
-                            else if (this.state.serviceDate===''){
-                                console.log("Di pde");
-                                this.setState({errorMsg:"* Please select date of service"})
-                                return
-                            }
-                            else if (this.state.actualDate.getTime() <= new Date().getTime()){
-                                console.log("Awit di pde");
-                                this.setState({errorMsg:"* Please select valid date of service"})
-                                return
-                            }
-                            else 
-                            {
-                                const user = firebase.auth().currentUser;
-                                var id = new Date().getTime().toString();   
-                                firebase
-                                    .database()
-                                    .ref('bookings/' + user['uid'] +'/'+ id)
-                                    .set({
-                                        id:id,
-                                        category:this.state.categoryValue,
-                                        service:this.state.serviceValue,
-                                        service_date:this.state.serviceDate,
-                                        service_price:this.state.servicePrice,
-                                        service_currency:this.state.serviceCurrency
-                                    });
-
-                                this.getTempBooking();
-                            }
-                        }}
+                    <View style={{backgroundColor:"white", justifyContent:'center'}}>
+                        <Text style={{marginLeft:10}}>Category</Text>
+                        <RNPickerSelect
+                            onValueChange={(value) => {
+                                this.setState({categoryValue:value})
+                                // this.setState({serviceValue:''});
+                                this.getServiceList(value);
+                                // const listService = this.getServiceList(value);
+                                // this.setState({services:listService});
+                            }}
+                            items={this.state.categories}
                         >
-                        <Text>Add service</Text>
-                    </TouchableOpacity>
-    
-                    <Text>{this.state.errorMsg}</Text>
+                            <Text style={{marginLeft:10}}>
+                                {this.state.categoryValue?this.state.categoryValue:'Select an item...'}
+                            </Text>
+                        </RNPickerSelect>
 
-                    {/* <TouchableOpacity 
-                        // style={{}}
-                        onPress={()=>{
-                            this.getTempBooking();
-                        }}
+                    </View>
+
+                    <View style={{backgroundColor:"white", justifyContent:'center'}}>
+                        <Text style={{marginLeft:10}}>Service</Text>
+                        <RNPickerSelect
+                            onValueChange={(value) => {
+                                console.log(value);
+                                this.setState({serviceValue:value});
+                            }}
+                            items={this.state.services}
                         >
-                        <Text>Refresh</Text>
-                    </TouchableOpacity> */}
+                            <Text style={{marginLeft:10}}>{this.state.serviceValue?this.state.serviceValue:'Select an item...'}</Text>
+                        </RNPickerSelect>
 
-                    <FlatList
-                        data={this.state.tempBookValue?this.state.tempBookValue:null}
-                        renderItem={item => this.renderItemComponent(item)}
-                        keyExtractor={item => item.id.toString()}
-                        // ItemSeparatorComponent={this.ItemSeparator}
-                        // refreshing={this.state.refreshing}
-                        // onRefresh={this.handleRefresh}
+                    </View>
+                    
+                    <View style={{backgroundColor:"white", justifyContent:'center'}}>
+                        <Text style={{marginLeft:10}}>Date of service</Text>
+                        <Text style={{marginLeft:10}}>{this.state.serviceDate?this.state.serviceDate:"Not set"}</Text>
+                        
+                        {/* <Button title="Set date of service" onPress={this.showDateTimePicker} /> */}
+                        <TouchableOpacity 
+                            style={{marginLeft:10}}
+                            onPress={this.showDateTimePicker}
+                            >
+                            <Text>Set date of service</Text>
+                        </TouchableOpacity>
+
+                        <DateTimePicker
+                            isVisible={this.state.isDateTimePickerVisible}
+                            onConfirm={this.handleDatePicked}
+                            onCancel={this.hideDateTimePicker}
+                            display="default"
                             />
+                    </View>
 
-                    <Text>Total price: PHP {this.state.totalServicePrice?this.state.totalServicePrice:0}</Text>
+                    <TouchableOpacity 
+                            // style={{}}
+                            onPress={()=>{
 
-                    <Text>Payment method</Text>
+                                this.setState({errorMsg:''})
 
-                    <RNPickerSelect
-                        onValueChange={(value) => {
-                            console.log(value);
-                            this.setState({paymentMethodValue:value});
-                        }}
-                        items={[
-                            { label: 'Cash', value: 'Cash' }
-                        ]}
-                    >
-                        <Text>{this.state.paymentMethodValue?this.state.paymentMethodValue:'Select an item...'}</Text>
-                    </RNPickerSelect>
+                                // console.log("date",this.state.serviceDate);  
+                                if (this.state.categoryValue===null){
+                                    console.log("Di pde");
+                                    this.setState({errorMsg:"* Please select category"})
+                                    return
+                                }
+                                else if (this.state.serviceValue===null){
+                                    console.log("Di pde");
+                                    this.setState({errorMsg:"* Please select service"})
+                                    return
+                                }
+                                else if (this.state.serviceDate===''){
+                                    console.log("Di pde");
+                                    this.setState({errorMsg:"* Please select date of service"})
+                                    return
+                                }
+                                else if (this.state.actualDate.getTime() <= new Date().getTime()){
+                                    console.log("Awit di pde");
+                                    this.setState({errorMsg:"* Please select valid date of service"})
+                                    return
+                                }
+                                else 
+                                {
+                                    const user = firebase.auth().currentUser;
+                                    var id = new Date().getTime().toString();   
+                                    firebase
+                                        .database()
+                                        .ref('bookings/' + user['uid'] +'/'+ id)
+                                        .set({
+                                            id:id,
+                                            category:this.state.categoryValue,
+                                            service:this.state.serviceValue,
+                                            service_date:this.state.serviceDate,
+                                            service_price:this.state.servicePrice,
+                                            service_currency:this.state.serviceCurrency
+                                        });
+
+                                    this.getTempBooking();
+                                }
+                            }}
+                            >
+                            <Text>Add service</Text>
+                        </TouchableOpacity>
+        
+                        <Text>{this.state.errorMsg}</Text>
+
+                        {/* <TouchableOpacity 
+                            // style={{}}
+                            onPress={()=>{
+                                this.getTempBooking();
+                            }}
+                            >
+                            <Text>Refresh</Text>
+                        </TouchableOpacity> */}
+
+                        <FlatList
+                            data={this.state.tempBookValue?this.state.tempBookValue:null}
+                            renderItem={item => this.renderItemComponent(item)}
+                            keyExtractor={item => item.id.toString()}
+                            // ItemSeparatorComponent={this.ItemSeparator}
+                            // refreshing={this.state.refreshing}
+                            // onRefresh={this.handleRefresh}
+                                />
+
+                        <Text>Total price: PHP {this.state.totalServicePrice?this.state.totalServicePrice:0}</Text>
+
+                        <Text>Payment method</Text>
+
+                        <RNPickerSelect
+                            onValueChange={(value) => {
+                                console.log(value);
+                                this.setState({paymentMethodValue:value});
+                            }}
+                            items={[
+                                { label: 'Cash', value: 'Cash' }
+                            ]}
+                        >
+                            <Text>{this.state.paymentMethodValue?this.state.paymentMethodValue:'Select an item...'}</Text>
+                        </RNPickerSelect>
+
+                </ScrollView>
 
             </SafeAreaView>
         )
