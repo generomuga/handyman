dea92bbf31265c4bf928d1a9627dcc7dfcd61582
Gene Romuga <generomuga12693@gmail.com>
2021-07-20 11:24:07 +0800

    Update error messages and added result variable

--- src/functions/validation.js
+++ src/functions/validation.js
@@ -2,32 +2,31 @@ class Validation {
 
     isEmailEmpty(text) {
         if (text.trim() === '') {
-            // console.log('Empty email new')
-            return true
+            return [true, 'Please input your email']
         }
         else {
-            return false
+            return [false, '']
         }
     }
 
-    isNotValidEmail(text) {
+    isEmailInvalid(text) {
         let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        
         if (pattern.test(text) === false) {
-            // console.log('Invalid email new')
-            return true;
+            return [true, 'Please input valid email'];
         }
         else {
-            return false
+            return [false, '']
         }
     }
 
     isPasswordEmpty(text) {
         if (text.trim() === '') {
             // console.log('Empty password new')
-            return true
+            return [true, 'Please input your password']
         }
         else {
-            return false
+            return [false, '']
         }
     }
 
