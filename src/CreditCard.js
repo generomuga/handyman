71852be3b20f50c01e8f40ce9498d50469e19ea7
Gene Romuga <generomuga12693@gmail.com>
2021-10-17 11:54:33 +0800

    #63 Add credit card payment method

--- src/CreditCard.js
+++ src/CreditCard.js
@@ -17,7 +17,133 @@ import {
 export default function CreditCard({ navigation, route }) {
   useEffect(() => {}, []);
 
-  const _onChange = (form) => console.log(form);
+  const [creditCardInfo, setCreditCardInfo] = useState("");
+  const [paymentMethodId, setPaymentMethodId] = useState("");
+  const [paymentIntentId, setPaymentIntentId] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // const _onChange = (form) => console.log(form);
+  const addZeros = (totalServicePrice) => {
+    return parseInt(totalServicePrice + "00");
+  };
+
+  const createPaymentMethod = async () => {
+    try {
+      setErrorMessage("");
+      let isValid = creditCardInfo["valid"];
+      console.log(isValid);
+      if (isValid != false) {
+        let amount = addZeros(route.params.amount);
+        console.log(creditCardInfo);
+
+        let card_no = creditCardInfo["values"]["number"]
+          .replace(/\s+/g, "")
+          .trim();
+        let cvc = creditCardInfo["values"]["cvc"];
+        let expiry = creditCardInfo["values"]["expiry"].split("/");
+        let month = parseInt(expiry[0]);
+        let year = parseInt("20" + expiry[1]);
+
+        const options = {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Basic c2tfdGVzdF9GNjlYb0U0UTI2WlZUZ0NNWlNpWmpSeEw6",
+          },
+          body: JSON.stringify({
+            data: {
+              attributes: {
+                details: {
+                  card_number: card_no,
+                  exp_month: month,
+                  exp_year: year,
+                  cvc: cvc,
+                },
+                type: "card",
+              },
+            },
+          }),
+        };
+
+        await fetch("https://api.paymongo.com/v1/payment_methods", options)
+          .then((response) => response.json())
+          .then((response) => {
+            let pid = response["data"]["id"];
+            console.log(pid);
+            setPaymentMethodId(pid);
+            const options = {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization:
+                  "Basic c2tfdGVzdF9GNjlYb0U0UTI2WlZUZ0NNWlNpWmpSeEw6",
+              },
+              body: JSON.stringify({
+                data: {
+                  attributes: {
+                    amount: amount,
+                    payment_method_allowed: ["card"],
+                    payment_method_options: {
+                      card: { request_three_d_secure: "any" },
+                    },
+                    currency: "PHP",
+                  },
+                },
+              }),
+            };
+
+            fetch("https://api.paymongo.com/v1/payment_intents", options)
+              .then((response) => response.json())
+              .then((response) => {
+                // console.log(response);
+                let piid = response["data"]["id"];
+                setPaymentIntentId(piid);
+
+                const options = {
+                  method: "POST",
+                  headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization:
+                      "Basic c2tfdGVzdF9GNjlYb0U0UTI2WlZUZ0NNWlNpWmpSeEw6",
+                  },
+                  body: JSON.stringify({
+                    data: {
+                      attributes: {
+                        payment_method: paymentMethodId,
+                      },
+                    },
+                  }),
+                };
+
+                fetch(
+                  "https://api.paymongo.com/v1/payment_intents/" +
+                    paymentIntentId +
+                    "/attach",
+                  options
+                )
+                  .then((response) => response.json())
+                  .then((response) => {
+                    console.log(response);
+                    // null;
+                  })
+                  .catch((err) => console.error(err));
+              })
+              .catch((err) => {
+                setErrorMessage("Please enter a valid card info");
+              });
+          })
+          .catch((err) => console.error(err));
+      } else {
+        console.log("Not valid");
+        setErrorMessage("Please enter a valid card info");
+      }
+    } catch (error) {
+    } finally {
+    }
+  };
 
   return (
     <View style={{ flex: 1 }}>
@@ -31,12 +157,23 @@ export default function CreditCard({ navigation, route }) {
           marginTop: 120,
         }}
       >
-        <CreditCardInput onChange={(form) => console.log(form)} />
+        <CreditCardInput
+          onChange={(form) => {
+            setCreditCardInfo(form);
+            // console.log(form);
+          }}
+        />
+      </View>
+      <View>
+        <Text>{errorMessage}</Text>
       </View>
       <View style={{ flex: 0.1 }}>
         <TouchableOpacity
           onPress={() => {
-            navigation.goBack();
+            // console.log(creditCardInfo);
+            // navigation.goBack();
+            // console.log(route.params.amount);
+            createPaymentMethod();
           }}
         >
           <Text>Login</Text>
